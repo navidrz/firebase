c07be2663618f2eb94419c2aae2189110cf11a66
navidrz <124895301+navidrz@users.noreply.github.com>
2023-02-24 16:24:03 +0300

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -1,14 +1,25 @@
-// Replace with your own firebase config!
-export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyBDsdN3XWM5D2-c_ti5j44GrhMnBHNmSF0',
-  authDomain: 'react-native-market-2861b.firebaseapp.com',
-  databaseURL: 'https://react-native-market-2861b.firebaseio.com',
-  projectId: 'react-native-market-2861b',
-  storageBucket: 'react-native-market-2861b.appspot.com',
-  messagingSenderId: '878215484396',
-  appId: '1:878215484396:web:c36074bccb0b5693b0a6c9',
-  measurementId: 'G-317PLP59P2',
-}
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD2Jcdy3IQAMQKIgE-4YloXikmgqF4vJYw",
+  authDomain: "ibx1-b26f5.firebaseapp.com",
+  databaseURL: "https://ibx1-b26f5-default-rtdb.firebaseio.com",
+  projectId: "ibx1-b26f5",
+  storageBucket: "ibx1-b26f5.appspot.com",
+  messagingSenderId: "435743056532",
+  appId: "1:435743056532:web:d89442af8b2d8640814a81",
+  measurementId: "G-9CBB5T5KJY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Replace with your own IDs! follow the guides here:
 // https://docs.expo.io/versions/latest/sdk/google/#using-it-inside-of-the-expo-app
