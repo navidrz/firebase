d056288820b965c03bad3a2b0b6f64026e51fa36
navidrz <124895301+navidrz@users.noreply.github.com>
2023-02-24 16:50:01 +0300

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -23,12 +23,6 @@ const analytics = getAnalytics(app);
 
 // Replace with your own IDs! follow the guides here:
 // https://docs.expo.io/versions/latest/sdk/google/#using-it-inside-of-the-expo-app
-export const ANDROID_GOOGLE_CLIENT_ID =
-  '878215484396-4pbcf3tghqe9sa0pvq3fifb5etnn4q6s.apps.googleusercontent.com'
-export const IOS_GOOGLE_CLIENT_ID =
-  '878215484396-5hiqfjetgvbdck03l0jepdgku6u64erm.apps.googleusercontent.com'
-
-// Replace with your own facebook app ID.
-// You can find more information how to generate one here:
-// https://docs.expo.io/versions/latest/sdk/facebook/#registering-your-app-with-facebook
-export const FACEBOOK_APP_ID = '820370405209413'
+export const GOOGLE_CLIENT_ID =
+  'AIzaSyAB3BDwhGBtCiU5hPiWQuc6ak19R_zjhk4'
+  
